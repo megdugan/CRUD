b906b764a222f867b3450546df4a206527666650
Meg Dugan <md109@wellesley.edu>
2025-11-04 20:08:45 -0500

    Added update function.

--- wmdb.py
+++ wmdb.py
@@ -7,7 +7,10 @@ import cs304dbi as dbi
 addedby = 123
 
 def find_tt(conn, tt):
-    '''Checks if the tt exists in the database.'''
+    '''
+    Checks if the tt exists in the database.
+    If none, returns empty dict. Otherwise, returns result.
+    '''
     curs = dbi.dict_cursor(conn)
     curs.execute('''select tt
                     from movie
@@ -15,7 +18,7 @@ def find_tt(conn, tt):
     return curs.fetchall()
 
 def insert_movie(conn, tt, movie):
-    '''Adds the submitted movie to the database'''
+    '''Adds the submitted movie to the database.'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''insert into movie (tt, title, addedby) 
                     values (%s, %s, %s)''', [tt, movie, addedby])
@@ -34,7 +37,17 @@ def get_incomplete_movies(conn):
     curs.execute('''select * from movie where 'release' is NULL or director is NULL''')
     return curs.fetchall()
 
+def update(conn, tt, release, director):
+    '''Updates the given movie with the submitted data.'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''update movie
+                    set `release`=%s, director=%s
+                    where tt=%s''', [release, director, tt])
+    conn.commit()
+    return
+
 if __name__ == '__main__':
-    dbi.conf("nh107_db")
+    dbi.conf("md109_db")
     conn = dbi.connect()
+    print(find_tt(conn, 123))
     print(get_incomplete_movies(conn)[:5])
