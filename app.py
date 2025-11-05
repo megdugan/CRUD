5155efdbf356d9736b00bfbea07244519f606c91
Meg Dugan <md109@wellesley.edu>
2025-11-04 19:45:56 -0500

    Updated base html and functions

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
+# Meg and Nina
+# CRUD assignment
+
 from flask import (Flask, render_template, make_response, url_for, request,
                    redirect, flash, session, send_from_directory, jsonify)
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
-
 import secrets
 import cs304dbi as dbi
 
-# we need a secret_key to use flash() and sessions
+app = Flask(__name__)
 app.secret_key = secrets.token_hex()
 
-# configure DBI
-
-# For Lookup, use 'wmdb'
-# For CRUD and Ajax, use your personal db
-# For project work, use your team db
-
-print(dbi.conf('ww123_db'))
+# Configures DBI
+print(dbi.conf('md109_db'))
 
 # This gets us better error messages for certain common request errors
 app.config['TRAP_BAD_REQUEST_ERRORS'] = True
 
 @app.route('/')
-def index():
+def main():
     return render_template('main.html', page_title='Main Page')
 
-@app.route('/about/')
-def about():
-    flash('this is a flashed message')
-    return render_template('about.html', page_title='About Us')
+@app.route('/insert/')
+def insert():
+    return render_template('insert.html')
+
+@app.route('/select/')
+def select():
+    return render_template('select.html')
 
 if __name__ == '__main__':
     import sys, os
