60908c6e95d1625f2b491327d739ff32774f2121
Meg Dugan <md109@wellesley.edu>
2025-11-04 19:57:53 -0500

    Insert database functions.

--- wmdb.py
+++ wmdb.py
@@ -1,1 +1,23 @@
+# Meg and Nina
+# CRUD assignment
+# Database functions
+
 import cs304dbi as dbi
+
+addedby = 123
+
+def find_tt(conn, tt):
+    '''Checks if the tt exists in the database.'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select tt
+                    from movie
+                    where tt=%s''', [tt])
+    return curs.fetchall()
+
+def insert_movie(conn, tt, movie):
+    '''Adds the submitted movie to the database'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into movie (tt, title, addedby) 
+                    values (%s, %s, %s)''', [tt, movie, addedby])
+    conn.commit()
+    return
